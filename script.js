ee6df9c05c544583866bb891297b2db31ed04fa5
praveen <praveenkpk6515@gmail.com>
2024-07-13 12:19:08 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,35 @@
+let input=document.querySelector("#input");
+let list=document.querySelector("#todo-list");
+function addTask(){
+    if(input.value === ''){
+        alert("Write the task and then Add..!");
+    }
+    else{
+        let li=document.createElement("li");
+        li.innerHTML=input.value;
+        list.appendChild(li);
+        let span=document.createElement("span");
+        span.innerHTML="\u00d7";
+        li.appendChild(span);
+    }
+    input.value='';
+    saveData();
+}
+list.addEventListener("click",function(e){
+    if(e.target.tagName === 'LI'){
+        e.target.classList.toggle("checked");
+        saveData();
+    }
+    else if(e.target.tagName === 'SPAN'){
+        e.target.parentElement.remove();
+        saveData();
+    }
+},false);
+
+function saveData(){
+    localStorage.setItem("data", list.innerHTML);
+}
+function showData(){
+    list.innerHTML=localStorage.getItem("data");
+}
+showData();
